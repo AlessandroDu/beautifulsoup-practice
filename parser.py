9c55f260bf901923178ca2f6c59cd91b5b5652c8
Alessandro Ducci <duccialessandro001@gmail.com>
2025-01-03 21:41:46 +0400

    Learned about the Tag Object

--- parser.py
+++ parser.py
@@ -1,11 +1,6 @@
 from bs4 import BeautifulSoup
-import requests
 
-
-url = "https://www.tutorialspoint.com/index.htm"
-req = requests.get(url)
-
-soup = BeautifulSoup(req.content, "lxml")
-
-for link in soup.find_all('a'):
-    print(link.get('href'))
+soup = BeautifulSoup('<b class="boldest">TutorialsPoint</b>', 'lxml')
+tag = soup.html
+tag.name = "strong"
+print (tag)
