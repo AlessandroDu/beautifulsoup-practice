b300338d7f568b6c44f0d9f8f4b36e306084ce6d
Alessandro Ducci <duccialessandro001@gmail.com>
2025-01-03 21:45:01 +0400

    Learned about the Tag attrs.

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 
 soup = BeautifulSoup('<b class="boldest">TutorialsPoint</b>', 'lxml')
-tag = soup.html
-tag.name = "strong"
-print (tag)
+tag = soup.b
+
+print (tag.attrs)
